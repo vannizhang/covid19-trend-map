675d6e57b95ed9bdf422e2f839295f5dcbcd9886
Jinnan Zhang <vannizhang@gmail.com>
2020-07-23 11:11:50 -0700

    update queryCovid19Data

--- src/utils/queryCovid19Data.ts
+++ src/utils/queryCovid19Data.ts
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-type Covid19CasesByTimeFeature = {
+export type Covid19CasesByTimeFeature = {
     attributes: {
         dt: string;
         Confirmed: number;
         Deaths: number;
         NewCases: number;
+        Population: number;
     }
 };
 
@@ -21,12 +22,48 @@ export const fetchCovid19Data = async({
     stateName
 }:FetchCovid19DataOptions):Promise<Covid19CasesByTimeFeature[]>=>{
 
-    const requestUrl = countyFIPS
-        ? `${USCountiesCovid19CasesByTimeFeatureServiceURL}/query/?f=json&where=FIPS=${countyFIPS}&outFields=dt,Confirmed,Deaths,NewCases`
-        : `${USCountiesCovid19CasesByTimeFeatureServiceURL}/query/?where=ST_Name+%3D+%27${stateName}%27&objectIds=&time=&resultType=none&outFields=*&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=dt&groupByFieldsForStatistics=ST_Name%2C+dt&outStatistics=%5B%0D%0A++%7B%0D%0A++++%22statisticType%22%3A+%22sum%22%2C%0D%0A++++%22onStatisticField%22%3A+%22Confirmed%22%2C+%0D%0A++++%22outStatisticFieldName%22%3A+%22Confirmed%22%0D%0A++%7D%2C%0D%0A++%7B%0D%0A++++%22statisticType%22%3A+%22sum%22%2C%0D%0A++++%22onStatisticField%22%3A+%22Deaths%22%2C+%0D%0A++++%22outStatisticFieldName%22%3A+%22Deaths%22%0D%0A++%7D%2C%0D%0A++%7B%0D%0A++++%22statisticType%22%3A+%22sum%22%2C%0D%0A++++%22onStatisticField%22%3A+%22NewCases%22%2C%0D%0A++++%22outStatisticFieldName%22%3A+%22NewCases%22%0D%0A++%7D++%0D%0A%5D&having=&resultOffset=&resultRecordCount=&sqlFormat=none&f=pjson&token=`;
-    
+    const requestUrl = `${USCountiesCovid19CasesByTimeFeatureServiceURL}/query`;
+
+    const params = countyFIPS
+        ? {
+            f: 'json',
+            where: `FIPS='${countyFIPS}'`,
+            outFields: 'dt,Confirmed,Deaths,NewCases,Population'
+        }
+        : {
+            f: 'json',
+            where: `ST_Name='${stateName}'`,
+            outFields: '*',
+            orderByFields: 'dt',
+            groupByFieldsForStatistics: 'ST_Name,dt',
+            outStatistics: JSON.stringify([
+                {
+                    "statisticType": "sum",
+                    "onStatisticField": "Confirmed", 
+                    "outStatisticFieldName": "Confirmed"
+                },
+                {
+                    "statisticType": "sum",
+                    "onStatisticField": "Deaths", 
+                    "outStatisticFieldName": "Deaths"
+                },
+                {
+                    "statisticType": "sum",
+                    "onStatisticField": "NewCases",
+                    "outStatisticFieldName": "NewCases"
+                },
+                {
+                    "statisticType": "sum",
+                    "onStatisticField": "Population",
+                    "outStatisticFieldName": "Population"
+                }  
+            ])
+        };
+
     try {
-        const {data} = await axios.get(requestUrl);
+        const {data} = await axios.get(requestUrl, { 
+            params: new URLSearchParams(params)
+        });
 
         if(data && data.features){
             // console.log(data.features)
