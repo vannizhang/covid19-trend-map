897ab232b14ba05ca61f470a19c1ad8f9c7addae
Jinnan Zhang <vannizhang@gmail.com>
2020-08-31 16:22:45 -0700

    update getModifiedTime

--- src/utils/getModifiedDate.ts
+++ src/utils/getModifiedDate.ts
@@ -2,13 +2,14 @@ import { subHours } from 'date-fns';
 
 export const getModifiedTime = ()=>{
     const now = new Date();
-    const syncHourInUTC = 13 
+    // const syncHourInUTC = 13 
     const utcYear = now.getUTCFullYear()
     const utcMonth = now.getUTCMonth()
     const utcDate = now.getUTCDate()
     const utcHour = now.getUTCHours()
-    // const utcMinutes = now.getUTCMinutes()
-    // console.log(now, utcYear, utcMonth, utcDate, utcHour, utcMinutes)
+
+    // the file can be modified at 13PM or 14PM UTC
+    let syncHourInUTC = utcHour === 13 ? utcHour : 14;
 
     let modifiedTime = new Date(Date.UTC(utcYear, utcMonth, utcDate, syncHourInUTC));
 
