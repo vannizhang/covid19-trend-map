2e24a4d0c47b462c072e6db4184cfaae0b247a3a
Jinnan Zhang <vannizhang@gmail.com>
2020-11-15 13:26:38 -0800

    minor bug fix for getNumberWithOrdinalIndicator

--- src/utils/getNumberWithOrdinalIndicator.ts
+++ src/utils/getNumberWithOrdinalIndicator.ts
@@ -1,13 +1,24 @@
-const OrdinalIndicatorLookup = ['st', 'nd', 'rd', 'th']
-
 export const getNumberWithOrdinalIndicator = (num:number):string => {
     if(!num){
         return '';
     }
 
     const lastDigit = num % 10;
+    const last2Digit = num % 100;
 
-    const ordinalIndicator = OrdinalIndicatorLookup[lastDigit - 1] || OrdinalIndicatorLookup[3];
+    let ordinalIndicator = 'th';
+
+    if(lastDigit === 1 || last2Digit !== 11){
+        ordinalIndicator = 'st'
+    }
+
+    if(lastDigit === 2 || last2Digit !== 12){
+        ordinalIndicator = 'nd'
+    }
+
+    if(lastDigit === 3 || last2Digit !== 13){
+        ordinalIndicator = 'rd'
+    }
 
     return `${num}${ordinalIndicator}`;
 }
